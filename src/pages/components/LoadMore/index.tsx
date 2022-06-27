6d435c0605cbbd5c1f4e869e3d45a042747bce67
EnochMeng <menglzong>
2022-06-27 22:21:39 +0800

    LoadMore

--- src/pages/components/LoadMore/index.tsx
+++ src/pages/components/LoadMore/index.tsx
@@ -1,23 +1,44 @@
 import { Component } from 'react'
 import { View, Text } from '@tarojs/components'
 import './index.scss'
+import { AtLoadMore } from 'taro-ui'
 
-export default class Loadmore extends Component {
+interface IState {
+  status: 'more' | 'loading' | 'noMore'
+}
+export default class Loadmore extends Component<{}, IState> {
 
-  componentWillMount () { }
+  constructor(props) {
+    super(props)
+    this.state = {
+      status: 'more'
+    }
+  }
 
-  componentDidMount () { }
-
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
+  handleClick() {
+    this.setState({
+      status: 'loading'
+    })
+    setTimeout(() => {
+      this.setState({
+        status: 'noMore'
+      })
+    }, 2000);
+  }
 
   render () {
     return (
       <View className='LoadMore'>
-        <Text>Hello world!</Text>
+        <View className={'content'}>
+          <View className={'line'}></View>
+          <Text className={'title'}>一般用法</Text>
+        </View>
+        <View>
+          <AtLoadMore
+            onClick={this.handleClick.bind(this)}
+            status={this.state.status}
+          />
+        </View>
       </View>
     )
   }
