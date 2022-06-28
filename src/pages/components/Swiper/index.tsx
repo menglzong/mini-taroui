213fe89f50eb8e84a42901b7fe87f6b7559c39a4
EnochMeng <menglzong>
2022-06-28 17:14:19 +0800

    banner

--- src/pages/components/Swiper/index.tsx
+++ src/pages/components/Swiper/index.tsx
@@ -2,23 +2,59 @@ import { Component } from 'react'
 import { View, Text } from '@tarojs/components'
 import './index.scss'
 import Taro, { getCurrentInstance } from '@tarojs/taro'
+import { Swiper, SwiperItem } from '@tarojs/components'
+import { AtForm, AtSwitch } from 'taro-ui'
 
 interface IProps {
   title?: string
 }
-export default class Swiper extends Component<IProps> {
+
+interface IState {
+  showDots: boolean
+}
+export default class EnochSwiper extends Component<IProps, IState> {
 
   instance: IProps | undefined = getCurrentInstance().router?.params
 
   constructor(props) {
     super(props)
+    this.state = {
+      showDots: true
+    }
     Taro.setNavigationBarTitle({title: this.instance?.title ?? ''})
   }
 
+  onChangeShowDots(value) {
+    this.setState({
+      showDots: value
+    })
+  }
+
   render () {
+    const { showDots } = this.state
     return (
       <View className='Swiper'>
-        <Text>Hello world!</Text>
+        <Swiper
+          className='test-h'
+          indicatorColor='#999'
+          indicatorActiveColor='#333'
+          vertical={false}
+          circular
+          indicatorDots={showDots}
+          autoplay>
+          <SwiperItem>
+            <View className='demo-text-1'>1</View>
+          </SwiperItem>
+          <SwiperItem>
+            <View className='demo-text-2'>2</View>
+          </SwiperItem>
+          <SwiperItem>
+            <View className='demo-text-3'>3</View>
+          </SwiperItem>
+        </Swiper>
+        <AtForm>
+          <AtSwitch title={'指示点'} checked={showDots} onChange={this.onChangeShowDots.bind(this)}></AtSwitch>
+        </AtForm>
       </View>
     )
   }
