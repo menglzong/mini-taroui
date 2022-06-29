25b77dfde587c509db9264b8efc26c39319ae182
EnochMeng <menglzong>
2022-06-29 17:01:59 +0800

    modal

--- src/pages/feedback/EnochModal/index.tsx
+++ src/pages/feedback/EnochModal/index.tsx
@@ -1,23 +1,24 @@
 import { Component } from 'react'
 import { View, Text } from '@tarojs/components'
 import './index.scss'
+import { AtActionSheet, AtActionSheetItem, AtButton } from 'taro-ui'
 
 export default class Enochmodal extends Component {
 
-  componentWillMount () { }
+  onClick() {
 
-  componentDidMount () { }
-
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
+  }
 
   render () {
     return (
       <View className='EnochModal'>
-        <Text>Hello world!</Text>
+        <View className={'content'}>
+          <View className={'line'}></View>
+          <Text className={'title'}>有无标题</Text>
+        </View>
+        <View className={'content'}>
+          <AtButton type={'primary'} onClick={this.onClick.bind(this)}>打开ActionSheet</AtButton>
+        </View>
       </View>
     )
   }
