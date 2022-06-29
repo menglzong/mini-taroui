eaef037038e02b53002872a941f7972034cbac56
EnochMeng <menglzong>
2022-06-29 15:56:07 +0800

    AtActionSheet

--- src/pages/feedback/EnochActionSheet/index.tsx
+++ src/pages/feedback/EnochActionSheet/index.tsx
@@ -1,23 +1,84 @@
 import { Component } from 'react'
 import { View, Text } from '@tarojs/components'
 import './index.scss'
+import { AtActionSheet, AtActionSheetItem, AtButton } from 'taro-ui'
 
-export default class Enochactionsheet extends Component {
+interface IState {
+  isOpened: boolean
+  isShow: boolean
+}
 
-  componentWillMount () { }
+export default class Enochactionsheet extends Component<{}, IState> {
 
-  componentDidMount () { }
+  constructor(props) {
+    super(props)
+    this.state = {
+      isOpened: false,
+      isShow: false
+    }
+  }
 
-  componentWillUnmount () { }
+  onClick() {
+    this.setState({
+      isOpened: true
+    })
+  }
 
-  componentDidShow () { }
+  onCustomClick() {
+    this.setState({
+      isShow: true
+    })
+  }
 
-  componentDidHide () { }
+  onCancelClick() {
+    this.setState({
+      isOpened: false,
+      isShow: false
+    })
+  }
 
   render () {
+    const { isOpened, isShow } = this.state
     return (
       <View className='EnochActionSheet'>
-        <Text>Hello world!</Text>
+        <AtActionSheet title={'有无标题'} isOpened={isOpened}>
+          <AtActionSheetItem onClick={this.onCancelClick.bind(this)}>
+            按钮一
+          </AtActionSheetItem>
+          <AtActionSheetItem onClick={this.onCancelClick.bind(this)}>
+            按钮二
+          </AtActionSheetItem>
+        </AtActionSheet>
+
+
+        <AtActionSheet title={'自定义内容'} isOpened={isShow} cancelText={'自带取消'}>
+          <AtActionSheetItem onClick={this.onCancelClick.bind(this)}>
+            按钮一
+          </AtActionSheetItem>
+          <AtActionSheetItem onClick={this.onCancelClick.bind(this)}>
+            按钮二
+          </AtActionSheetItem>
+          <AtActionSheetItem onClick={this.onCancelClick.bind(this)}>
+            <Text className={'custom'}>自定义按钮</Text>
+          </AtActionSheetItem>
+        </AtActionSheet>
+
+        <View className={'content'}>
+          <View className={'line'}></View>
+          <Text className={'title'}>有无标题</Text>
+        </View>
+        <View className={'content'}>
+          <AtButton type={'primary'} onClick={this.onClick.bind(this)}>打开ActionSheet</AtButton>
+        </View>
+
+        <View className={'content'}>
+          <View className={'line'}></View>
+          <Text className={'title'}>自定义内容</Text>
+        </View>
+        <View className={'content'}>
+          <AtButton type={'primary'} onClick={this.onCustomClick.bind(this)}>打开ActionSheet</AtButton>
+        </View>
+
       </View>
     )
   }
