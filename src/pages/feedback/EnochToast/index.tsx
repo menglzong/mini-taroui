63eca10a15414947123da75698e13af35fc4f451
EnochMeng <menglzong>
2022-06-29 10:01:14 +0800

    文件建立

---
+++ src/pages/feedback/EnochToast/index.tsx
@@ -0,0 +1,24 @@
+import { Component } from 'react'
+import { View, Text } from '@tarojs/components'
+import './index.scss'
+
+export default class Enochtoast extends Component {
+
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='EnochToast'>
+        <Text>Hello world!</Text>
+      </View>
+    )
+  }
+}
