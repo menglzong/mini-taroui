2621f769043289fdebb0cb7683f57c3da9329284
EnochMeng <menglzong>
2022-07-09 18:05:31 +0800

    Message

--- src/pages/feedback/EnochMessage/index.tsx
+++ src/pages/feedback/EnochMessage/index.tsx
@@ -1,13 +1,32 @@
 import { Component } from 'react'
 import { View, Text } from '@tarojs/components'
 import './index.scss'
-
+import Taro from '@tarojs/taro'
+import { AtMessage, AtButton } from 'taro-ui'
 export default class Enochmessage extends Component {
+
+  handleClick (type) {
+    Taro.atMessage({
+      'message': '消息通知' + (typeof(type) == 'object' ? '' : type),
+      'type': type,
+    })
+  }
 
   render () {
     return (
       <View className='EnochMessage'>
-        <Text>Hello world!</Text>
+        <AtMessage/>
+        <View className={'content'}>
+          <View className={'line'}></View>
+          <Text className={'title'}>基本案例</Text>
+        </View>
+        <View className={'padding'}>
+          <AtButton className={'padding'} onClick={this.handleClick.bind(this)}>普通消息</AtButton>
+          <AtButton className={'padding'} onClick={this.handleClick.bind(this, 'success')}>成功消息</AtButton>
+          <AtButton className={'padding'} onClick={this.handleClick.bind(this, 'error')}>错误消息</AtButton>
+          <AtButton className={'padding'} onClick={this.handleClick.bind(this, 'warning')}>警告消息</AtButton>
+          <AtButton className={'padding'} onClick={this.handleClick.bind(this, 'info')}>提示消息</AtButton>
+        </View>
       </View>
     )
   }
