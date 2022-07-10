ba5c00454e29a1ec46aab023357cbc0f6ef389a2
EnochMeng <menglzong>
2022-07-10 16:16:19 +0800

    表单

--- src/pages/formComponents/EnochForm/index.tsx
+++ src/pages/formComponents/EnochForm/index.tsx
@@ -1,23 +1,60 @@
 import { Component } from 'react'
 import { View, Text } from '@tarojs/components'
 import './index.scss'
+import { AtForm, AtInput, AtButton } from 'taro-ui'
+import Taro from '@tarojs/taro'
 
-export default class Enochform extends Component {
+interface IState {
+  name: string
+  pwd: string
+}
+export default class Enochform extends Component<{}, IState> {
 
-  componentWillMount () { }
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      pwd: ''
+    }
+  }
 
-  componentDidMount () { }
+  handleClick() {
+    const { name, pwd } = this.state
+    Taro.showToast({
+      title: '文本：' + name + '密码：' + pwd
+    })
+  }
 
-  componentWillUnmount () { }
+  handleResetClick() {
+    this.setState({
+      name: '',
+      pwd: ''
+    })
+  }
 
-  componentDidShow () { }
-
-  componentDidHide () { }
+  onChange(type, event) {
+    this.setState({
+      name: type === 'name' ? event : this.state.name,
+      pwd: type === 'value' ? event : this.state.pwd,
+    })
+  }
 
   render () {
+    const { name, pwd } = this.state
     return (
       <View className='EnochForm'>
-        <Text>Hello world!</Text>
+        <View className={'content'}>
+          <View className={'line'}></View>
+          <Text className={'title'}>表单提交与重置</Text>
+        </View>
+        <View className={'padding'}>
+          <AtForm>
+            <AtInput name={'name'} title={'文本'} placeholder={'请输入文本'} value={name} onChange={this.onChange.bind(this, 'name')}/>
+            <AtInput name={'value'} title={'密码'} placeholder={'请输入密码'} value={pwd} onChange={this.onChange.bind(this, 'value')}/>
+            <AtButton className={'padding'} onClick={this.handleClick.bind(this)}>提交</AtButton>
+            <AtButton className={'padding'} onClick={this.handleResetClick.bind(this)}>重置</AtButton>
+          </AtForm>
+        </View>
       </View>
     )
   }
