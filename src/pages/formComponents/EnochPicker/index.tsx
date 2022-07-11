ff2486615fb732cbc9e5ad255375e93daf37cfab
EnochMeng <menglzong>
2022-07-11 17:34:00 +0800

    选择器

--- src/pages/formComponents/EnochPicker/index.tsx
+++ src/pages/formComponents/EnochPicker/index.tsx
@@ -1,23 +1,62 @@
 import { Component } from 'react'
 import { View, Text } from '@tarojs/components'
 import './index.scss'
+import { Picker } from '@tarojs/components'
+import { AtList, AtListItem } from 'taro-ui'
 
-export default class Enochpicker extends Component {
+interface IState {
+  selector: string[][]
+  selectorChecked: string
+  selChecked: number[]
+  timeSel: string
+  dateSel: string
+}
+export default class Enochpicker extends Component<{}, IState> {
 
-  componentWillMount () { }
+  constructor(props) {
+    super(props)
+    this.state = {
+      selector: [['美国', '中国', '巴西', '日本'], ['s', 'f', 'g']],
+      selectorChecked: '美国',
+      timeSel: '12:01',
+      dateSel: '2018-04-22',
+      selChecked: [0, 0]
+    }
+  }
 
-  componentDidMount () { }
+  onChange(event) {
+    const { selector } = this.state
+    let array = event.detail.value
+    let one = selector[0][array[0]]
+    let two = selector[1][array[1]]
+    this.setState({
+      selChecked: array,
+      selectorChecked: one + two
+    })
+  }
 
-  componentWillUnmount () { }
+  onChanging(event) {
 
-  componentDidShow () { }
-
-  componentDidHide () { }
+  }
 
   render () {
+    const { selector, selChecked } = this.state
     return (
       <View className='EnochPicker'>
-        <Text>Hello world!</Text>
+        <View className={'content'}>
+          <View className={'line'}></View>
+          <Text className={'title'}>多列选择器</Text>
+        </View>
+        <View className={'padding'}>
+          <Picker mode={'multiSelector'} value={selChecked} range={selector} onChange={this.onChange.bind(this)}>
+            <AtList>
+                <AtListItem
+                  title='国家地区'
+                  extraText={this.state.selectorChecked}
+                />
+              </AtList>
+          </Picker>
+        </View>
       </View>
     )
   }
