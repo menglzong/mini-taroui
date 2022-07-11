7a1323cd09f0ee1ca7141df19321c3ed8557ddda
EnochMeng <menglzong>
2022-07-11 14:30:49 +0800

    复选

--- src/pages/formComponents/EnochCheckbox/index.tsx
+++ src/pages/formComponents/EnochCheckbox/index.tsx
@@ -1,23 +1,44 @@
 import { Component } from 'react'
 import { View, Text } from '@tarojs/components'
 import './index.scss'
+import { AtCheckbox } from 'taro-ui'
 
-export default class Enochcheckbox extends Component {
+interface IState {
+  selectedList: number[]
+}
+export default class Enochcheckbox extends Component<{}, IState> {
 
-  componentWillMount () { }
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedList: []
+    }
+  }
 
-  componentDidMount () { }
-
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
+  onChange(event) {
+   this.setState({
+    selectedList: event
+   })
+  }
 
   render () {
     return (
-      <View className='EnochCheckbox'>
-        <Text>Hello world!</Text>
+      <View className='Enochcheckbox'>
+        <View className={'content'}>
+          <View className={'line'}></View>
+          <Text className={'title'}>基本用法</Text>
+        </View>
+        <View className={'padding'}>
+          <AtCheckbox
+            options={[
+              { label: '单选项一', value: 'option1', desc: '单选项描述' },
+              { label: '单选项二', value: 'option2' },
+              { label: '单选项三禁用', value: 'option3', desc: '单选项描述', disabled: true }
+            ]}
+            selectedList={this.state.selectedList}
+            onChange={this.onChange.bind(this)}
+          />
+        </View>
       </View>
     )
   }
--- src/pages/formComponents/EnochRadio/index.tsx
+++ src/pages/formComponents/EnochRadio/index.tsx
@@ -1,23 +1,44 @@
 import { Component } from 'react'
 import { View, Text } from '@tarojs/components'
 import './index.scss'
+import { AtRadio } from 'taro-ui'
 
-export default class Enochradio extends Component {
+interface IState {
+  value: number
+}
+export default class Enochradio extends Component<{}, IState> {
 
-  componentWillMount () { }
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: 0
+    }
+  }
 
-  componentDidMount () { }
-
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
+  onChange(event) {
+   this.setState({
+     value: event
+   })
+  }
 
   render () {
     return (
       <View className='EnochRadio'>
-        <Text>Hello world!</Text>
+        <View className={'content'}>
+          <View className={'line'}></View>
+          <Text className={'title'}>基本用法</Text>
+        </View>
+        <View className={'padding'}>
+          <AtRadio
+            options={[
+              { label: '单选项一', value: 'option1', desc: '单选项描述' },
+              { label: '单选项二', value: 'option2' },
+              { label: '单选项三禁用', value: 'option3', desc: '单选项描述', disabled: true }
+            ]}
+            value={this.state.value}
+            onClick={this.onChange.bind(this)}
+          />
+        </View>
       </View>
     )
   }
