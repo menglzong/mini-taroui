28b6082e5acfa986f1f1fa25c89a42b5f6ad6554
EnochMeng <menglzong>
2022-07-11 14:15:59 +0800

    数字输入框

--- src/pages/formComponents/EnochInputNumber/index.tsx
+++ src/pages/formComponents/EnochInputNumber/index.tsx
@@ -1,23 +1,39 @@
 import { Component } from 'react'
 import { View, Text } from '@tarojs/components'
 import './index.scss'
+import { AtInputNumber } from 'taro-ui'
 
-export default class Enochinputnumber extends Component {
+interface IState {
+  value: string
+}
 
-  componentWillMount () { }
+export default class Enochinputnumber extends Component<{}, IState> {
 
-  componentDidMount () { }
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: '0'
+    }
+  }
 
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
+  onChange(event) {
+    this.setState({
+      value: event
+    })
+    return event
+  }
 
   render () {
+    const { value } = this.state
     return (
       <View className='EnochInputNumber'>
-        <Text>Hello world!</Text>
+        <View className={'content'}>
+          <View className={'line'}></View>
+          <Text className={'title'}>基本用法</Text>
+        </View>
+        <View className={'padding'}>
+          <AtInputNumber type={'number'} min={0} max={10} step={1} value={value} onChange={this.onChange.bind(this)}/>
+        </View>
       </View>
     )
   }
