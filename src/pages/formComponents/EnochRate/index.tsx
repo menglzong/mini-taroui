51a30d411160a8d1a1a980b27664d5fb3112d71e
EnochMeng <menglzong>
2022-07-11 14:54:49 +0800

    Switch

--- src/pages/formComponents/EnochRate/index.tsx
+++ src/pages/formComponents/EnochRate/index.tsx
@@ -1,23 +1,40 @@
 import { Component } from 'react'
 import { View, Text } from '@tarojs/components'
 import './index.scss'
+import { AtRate } from 'taro-ui'
 
-export default class Enochrate extends Component {
+interface IState {
+  value: number
+}
+export default class Enochrate extends Component<{}, IState> {
 
-  componentWillMount () { }
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: 2
+    }
+  }
 
-  componentDidMount () { }
-
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
+  onChange(event) {
+   this.setState({
+    value: event
+   })
+  }
 
   render () {
     return (
-      <View className='EnochRate'>
-        <Text>Hello world!</Text>
+      <View className='Enochrate'>
+        <View className={'content'}>
+          <View className={'line'}></View>
+          <Text className={'title'}>基本用法</Text>
+        </View>
+        <View className={'padding'}>
+          <AtRate
+            value={this.state.value}
+            max={10}
+            onChange={this.onChange.bind(this)}
+          />
+        </View>
       </View>
     )
   }
--- src/pages/formComponents/EnochSwitch/index.tsx
+++ src/pages/formComponents/EnochSwitch/index.tsx
@@ -1,23 +1,42 @@
 import { Component } from 'react'
 import { View, Text } from '@tarojs/components'
 import './index.scss'
+import { AtSwitch } from 'taro-ui'
 
-export default class Enochswitch extends Component {
+interface IState {
+  value: boolean
+}
+export default class Enochswitch extends Component<{}, IState> {
 
-  componentWillMount () { }
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: false
+    }
+  }
 
-  componentDidMount () { }
-
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
+  onChange(event) {
+   this.setState({
+    value: event
+   })
+  }
 
   render () {
+    const { value } = this.state
+    let title = '状态:' + (value ? '开' : '关')
     return (
       <View className='EnochSwitch'>
-        <Text>Hello world!</Text>
+        <View className={'content'}>
+          <View className={'line'}></View>
+          <Text className={'title'}>基本用法</Text>
+        </View>
+        <View className={'padding'}>
+          <AtSwitch
+            title={title}
+            checked={value}
+            onChange={this.onChange.bind(this)}
+          />
+        </View>
       </View>
     )
   }
