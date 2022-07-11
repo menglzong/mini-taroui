5863d4794a3a295f0edeb15aa4d3c91e3d4ef821
EnochMeng <menglzong>
2022-07-11 15:39:57 +0800

    多行文本

--- src/pages/formComponents/EnochTextarea/index.tsx
+++ src/pages/formComponents/EnochTextarea/index.tsx
@@ -1,23 +1,42 @@
 import { Component } from 'react'
 import { View, Text } from '@tarojs/components'
 import './index.scss'
+import { AtTextarea } from 'taro-ui'
 
-export default class Enochtextarea extends Component {
+interface IState {
+  value: string
+}
+export default class Enochtextarea extends Component<{}, IState> {
 
-  componentWillMount () { }
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: ''
+    }
+  }
 
-  componentDidMount () { }
-
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
+  onChange(event) {
+   this.setState({
+    value: event
+   })
+  }
 
   render () {
+    const { value } = this.state
     return (
-      <View className='EnochTextarea'>
-        <Text>Hello world!</Text>
+      <View className='Enochtextarea'>
+        <View className={'content'}>
+          <View className={'line'}></View>
+          <Text className={'title'}>基本用法</Text>
+        </View>
+        <View className={'padding'}>
+          <AtTextarea
+            value={value}
+            maxLength={200}
+            placeholder={'请输入文本'}
+            onChange={this.onChange.bind(this)}
+          />
+        </View>
       </View>
     )
   }
