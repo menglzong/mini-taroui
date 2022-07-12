fb3bc611fc2a791716544035103ebfd266d605c5
EnochMeng <menglzong>
2022-07-12 08:59:43 +0800

    图片选择器

--- src/pages/formComponents/EnochImagePicker/index.tsx
+++ src/pages/formComponents/EnochImagePicker/index.tsx
@@ -1,23 +1,40 @@
 import { Component } from 'react'
 import { View, Text } from '@tarojs/components'
 import './index.scss'
+import { AtImagePicker } from 'taro-ui'
 
-export default class Enochimagepicker extends Component {
+interface IState {
+  files: any[]
+}
 
-  componentWillMount () { }
+export default class Enochimagepicker extends Component<{}, IState> {
 
-  componentDidMount () { }
+  constructor(props) {
+    super(props)
+    this.state = {
+      files:[]
+    }
+  }
 
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
+  onChange(event) {
+    console.log(event)
+    this.setState({
+      files: event
+    })
+    return event
+  }
 
   render () {
+    const { files } = this.state
     return (
-      <View className='EnochImagePicker'>
-        <Text>Hello world!</Text>
+      <View className='Enochimagepicker'>
+        <View className={'content'}>
+          <View className={'line'}></View>
+          <Text className={'title'}>基本用法</Text>
+        </View>
+        <View className={'padding'}>
+          <AtImagePicker files={files} onChange={this.onChange.bind(this)}/>
+        </View>
       </View>
     )
   }
