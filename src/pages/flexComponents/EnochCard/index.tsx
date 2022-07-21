f94b5c27f79d47546adb01a5bce3a392afb640b3
EnochMeng <menglzong>
2022-07-21 16:40:41 +0800

    栅格布局

--- src/pages/flexComponents/EnochCard/index.tsx
+++ src/pages/flexComponents/EnochCard/index.tsx
@@ -1,23 +1,13 @@
 import { Component } from 'react'
 import { View, Text } from '@tarojs/components'
 import './index.scss'
+import { AtCard } from 'taro-ui'
 
 export default class Enochcard extends Component {
-
-  componentWillMount () { }
-
-  componentDidMount () { }
-
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
 
   render () {
     return (
       <View className='EnochCard'>
-        <Text>Hello world!</Text>
       </View>
     )
   }
