06eb75e7f4d356e77b153adbb684772e91d4781b
EnochMeng <menglzong>
2022-08-03 15:15:36 +0800

    分页器

--- src/pages/navComponents/EnochPagination/index.tsx
+++ src/pages/navComponents/EnochPagination/index.tsx
@@ -1,7 +1,9 @@
 import { Component } from 'react'
 import { View, Text } from '@tarojs/components'
 import './index.scss'
-
+import { AtPagination } from 'taro-ui'
+import Taro from '@tarojs/taro'
+import { PageChangeData } from 'taro-ui/types/pagination'
 export default class Enochpagination extends Component {
 
   componentWillMount () { }
@@ -12,12 +14,39 @@ export default class Enochpagination extends Component {
 
   componentDidShow () { }
 
-  componentDidHide () { }
+  onPageChange (data: PageChangeData) {
+    Taro.showToast({
+      title: data.type + '-----'+ data.current
+    })
+  }
 
   render () {
     return (
       <View className='EnochPagination'>
-        <Text>Hello world!</Text>
+          <View className={'content'}>
+            <View className={'line'}></View>
+            <Text className={'title'}>基础用法</Text>
+          </View>
+          <AtPagination 
+            total={50} 
+            pageSize={1}
+            current={1}
+            onPageChange={this.onPageChange.bind(this)}
+          >
+          </AtPagination>
+
+          <View className={'content'}>
+            <View className={'line'}></View>
+            <Text className={'title'}>图标用法</Text>
+          </View>
+          <AtPagination 
+            icon
+            total={50} 
+            pageSize={1}
+            current={1}
+            onPageChange={this.onPageChange.bind(this)}
+          >
+          </AtPagination>
       </View>
     )
   }
