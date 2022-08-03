d1fb75f3bb3423a0c0367ca3350e5ca332096f31
EnochMeng <menglzong>
2022-08-03 14:29:45 +0800

    分段器

--- src/pages/navComponents/EnochSegmentedControl/index.tsx
+++ src/pages/navComponents/EnochSegmentedControl/index.tsx
@@ -1,10 +1,22 @@
 import { Component } from 'react'
 import { View, Text } from '@tarojs/components'
 import './index.scss'
+import { AtSegmentedControl }  from 'taro-ui'
 
-export default class Enochsegmentedcontrol extends Component {
 
-  componentWillMount () { }
+interface IState {
+  current: number
+  vCurrent: number
+}
+export default class Enochsegmentedcontrol extends Component<{}, IState> {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      current: 0,
+      vCurrent: 0
+    }
+  }
 
   componentDidMount () { }
 
@@ -12,12 +24,74 @@ export default class Enochsegmentedcontrol extends Component {
 
   componentDidShow () { }
 
-  componentDidHide () { }
+  handleClick (index: number) {
+    this.setState({
+      current: index
+    })
+  }
+
+  handleVClick (index: number) {
+    this.setState({
+      vCurrent: index
+    })
+  }
 
   render () {
     return (
       <View className='EnochSegmentedControl'>
-        <Text>Hello world!</Text>
+        <View className={'content'}>
+          <View className={'line'}></View>
+          <Text className={'title'}>基础用法</Text>
+        </View>
+
+        <AtSegmentedControl
+          values={['标签页1', '标签页2', '标签页3']}
+          onClick={this.handleClick.bind(this)}
+          current={this.state.current}
+        />
+        {
+          this.state.current === 0
+          ? <View className='tab-content'>标签1的内容</View>
+          : null
+        }
+        {
+          this.state.current === 1
+          ? <View className='tab-content'>标签2的内容</View>
+          : null
+        }
+        {
+          this.state.current === 2
+          ? <View className='tab-content'>标签3的内容</View>
+          : null
+        }
+
+        <View className={'content'}>
+          <View className={'line'}></View>
+          <Text className={'title'}>自定义颜色、字体大小</Text>
+        </View>
+
+        <AtSegmentedControl
+          values={['标签页1', '标签页2', '标签页3']}
+          onClick={this.handleVClick.bind(this)}
+          current={this.state.vCurrent}
+          selectedColor={'#FF4949'}
+          fontSize={30}
+        />
+        {
+          this.state.vCurrent === 0
+          ? <View className='tab-content'>标签1的内容</View>
+          : null
+        }
+        {
+          this.state.vCurrent === 1
+          ? <View className='tab-content'>标签2的内容</View>
+          : null
+        }
+        {
+          this.state.vCurrent === 2
+          ? <View className='tab-content'>标签3的内容</View>
+          : null
+        }
       </View>
     )
   }
--- src/pages/navComponents/EnochTabs/index.tsx
+++ src/pages/navComponents/EnochTabs/index.tsx
@@ -83,7 +83,7 @@ export default class Enochtabs extends Component<{}, IState> {
           current={this.state.vCurrent}
           scroll
           tabDirection={'vertical'}
-          height={'150'}
+          height={'150px'}
           tabList={[
             { title: '标签页1' },
             { title: '标签页2' },
@@ -94,22 +94,22 @@ export default class Enochtabs extends Component<{}, IState> {
           ]}
           onClick={this.handleVClick.bind(this)}>
           <AtTabsPane tabDirection='vertical' current={this.state.vCurrent} index={0}>
-            <View style='font-size:18px;text-align:center;height:100px;'>标签页一的内容</View>
+            <View style='font-size:18px;text-align:center;height:150px;'>标签页一的内容</View>
           </AtTabsPane>
           <AtTabsPane tabDirection='vertical' current={this.state.vCurrent} index={1}>
-            <View style='font-size:18px;text-align:center;height:100px;'>标签页二的内容</View>
+            <View style='font-size:18px;text-align:center;height:150px;'>标签页二的内容</View>
           </AtTabsPane>
           <AtTabsPane tabDirection='vertical' current={this.state.vCurrent} index={2}>
-            <View style='font-size:18px;text-align:center;height:100px;'>标签页三的内容</View>
+            <View style='font-size:18px;text-align:center;height:150px;'>标签页三的内容</View>
           </AtTabsPane>
           <AtTabsPane tabDirection='vertical' current={this.state.vCurrent} index={3}>
-            <View style='font-size:18px;text-align:center;height:100px;'>标签页四的内容</View>
+            <View style='font-size:18px;text-align:center;height:150px;'>标签页四的内容</View>
           </AtTabsPane>
           <AtTabsPane tabDirection='vertical' current={this.state.vCurrent} index={4}>
-            <View style='font-size:18px;text-align:center;height:100px;'>标签页五的内容</View>
+            <View style='font-size:18px;text-align:center;height:150px;'>标签页五的内容</View>
           </AtTabsPane>
           <AtTabsPane tabDirection='vertical' current={this.state.vCurrent} index={5}>
-            <View style='font-size:18px;text-align:center;height:100px;'>标签页六的内容</View>
+            <View style='font-size:18px;text-align:center;height:150px;'>标签页六的内容</View>
           </AtTabsPane>
         </AtTabs>
       </View>
