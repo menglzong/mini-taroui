fa615a1d4cef24eef4f5f865cc168ef8de5f5ba5
EnochMeng <menglzong>
2022-08-03 16:54:57 +0800

    抽屉

--- src/pages/navComponents/EnochDrawer/index.tsx
+++ src/pages/navComponents/EnochDrawer/index.tsx
@@ -1,23 +1,101 @@
 import { Component } from 'react'
 import { View, Text } from '@tarojs/components'
 import './index.scss'
+import { AtDrawer, AtButton, AtIcon } from 'taro-ui'
 
-export default class Enochdrawer extends Component {
 
-  componentWillMount () { }
+interface IState {
+  showLeft: boolean
+  showRight: boolean
+  showCustom: boolean
+}
+
+export default class Enochdrawer extends Component<{}, IState> {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      showLeft: false,
+      showRight: false,
+      showCustom: false
+    }
+  }
 
   componentDidMount () { }
 
-  componentWillUnmount () { }
+  onShow () {
+    this.setState({
+      showCustom: true
+    })
+  }
 
-  componentDidShow () { }
+  onClose () {
+    this.setState({
+      showLeft: false,
+      showRight: false,
+      showCustom: false
+    })
+  }
 
-  componentDidHide () { }
+  onLeftClick () { 
+    this.setState({
+      showLeft: true
+    })
+  }
+
+  onRightClick () { 
+    this.setState({
+      showRight: true
+    })
+  }
 
   render () {
     return (
       <View className='EnochDrawer'>
-        <Text>Hello world!</Text>
+
+        <AtDrawer 
+          show={this.state.showLeft} 
+          mask 
+          onClose={this.onClose.bind(this)} 
+          items={['菜单1', '菜单2']}
+        ></AtDrawer>
+        <View className={'content'}>
+          <View className={'line'}></View>
+          <Text className={'title'}>左侧滑出</Text>
+        </View>
+        <AtButton onClick={this.onLeftClick.bind(this)}>显示Drawer 抽屉</AtButton>
+
+
+        <AtDrawer 
+          right
+          show={this.state.showRight} 
+          mask 
+          onClose={this.onClose.bind(this)} 
+          items={['菜单1', '菜单2']}
+        ></AtDrawer>
+        <View className={'content'}>
+          <View className={'line'}></View>
+          <Text className={'title'}>右侧滑出</Text>
+        </View>
+        <AtButton onClick={this.onLeftClick.bind(this)}>显示Drawer 抽屉</AtButton>
+
+        <AtDrawer 
+          right
+          show={this.state.showCustom} 
+          mask 
+          onClose={this.onClose.bind(this)} 
+        >
+          <View className='drawer-item'>优先展示items里的数据</View>
+          <View className='drawer-item'>如果items没有数据就会展示children</View>
+          <View className='drawer-item'>这是自定义内容 <AtIcon value='home' size='20' /></View>
+          <View className='drawer-item'>这是自定义内容</View>
+        </AtDrawer>
+        <View className={'content'}>
+          <View className={'line'}></View>
+          <Text className={'title'}>右侧滑出自定义内容</Text>
+        </View>
+        <AtButton onClick={this.onShow.bind(this)}>显示Drawer 抽屉</AtButton>
+
       </View>
     )
   }
