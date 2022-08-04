868296a6451a428999618936d2f3fff3cb35f2f6
EnochMeng <menglzong>
2022-08-04 10:27:55 +0800

    索引

--- src/pages/navComponents/EnochIndexes/index.tsx
+++ src/pages/navComponents/EnochIndexes/index.tsx
@@ -1,7 +1,7 @@
 import { Component } from 'react'
 import { View, Text } from '@tarojs/components'
 import './index.scss'
-
+import { AtIndexes } from 'taro-ui'
 export default class Enochindexes extends Component {
 
   componentWillMount () { }
@@ -12,12 +12,41 @@ export default class Enochindexes extends Component {
 
   componentDidShow () { }
 
-  componentDidHide () { }
+  onClick () { }
 
   render () {
+    const list = [{
+      title: 'A',
+      key: 'A',
+      items: [
+        {
+          'name': '阿坝'
+          // 此处可加其他业务字段
+        },
+        {
+          'name': '阿拉善'
+        }]
+      },
+      {
+        title: 'B',
+        key: 'B',
+        items: [
+          {
+            'name': '北京'
+          },
+          {
+            'name': '保定'
+          }]
+      }
+    ]
     return (
       <View className='EnochIndexes'>
-        <Text>Hello world!</Text>
+        <AtIndexes
+          list={list}
+          onClick={this.onClick.bind(this)}
+        >
+          <View>自定义内容</View>
+        </AtIndexes>
       </View>
     )
   }
