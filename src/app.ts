3addb1264163d8c13f778c185cb2662fd4931375
EnochMeng <menglzong>
2022-08-30 11:04:54 +0800

    Revert "Revert "s""
    This reverts commit 2a2113628ab01bc642bf76bfc65e7eee91ad2cad.

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,11 @@ import './app.scss'
 import 'taro-ui/dist/style/index.scss'
 class App extends Component {
 
-  // componentDidMount () {}
+hahahahahah() {
 
-  // componentDidShow () {}
+}
 
-  // componentDidHide () {}
-
-  // componentDidCatchError () {}
-
-  // this.props.children 是将要会渲染的页面
-  render () {
+render () {
     return this.props.children
   }
 }
