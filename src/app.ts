6ba420e027c1287e379bb8802914f4e2f345acdb
EnochMeng <menglzong>
2022-08-30 14:00:59 +0800

    1

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,11 @@ import 'taro-ui/dist/style/index.scss'
 class App extends Component {
 
 hahahahahah() {
-  console.log(1111);
-  
-
+  console.log(1111)
 }
 
-
-
   render () {
-
+    return this.props.children
   }
 }
 
